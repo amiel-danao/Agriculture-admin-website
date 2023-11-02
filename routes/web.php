6e91ea55675d7e4b45c759edeb89c03151c58107
John Erick Arzaga <arzagaerick11@gmail.com>
2023-11-02 15:28:11 +0800

    done task #13, #14

--- routes/web.php
+++ routes/web.php
@@ -19,37 +19,46 @@ Route::redirect('/', 'login');
 
 Route::match(['get', 'post'], '/', function () {
     return view('pages.dashboard');
-})->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/dashboard', function () {
     return view('pages.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// routes for profile pages
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
+// routes for users pages
 Route::get('/users', function () {
     return view('pages.users'); 
-})->name('users');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::delete('/users', [UserController::class, 'delete'])->name('users.delete');
-Route::post('/users/update', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/fetchdata', [UserController::class, 'fetchdata'])->name('users.fetchdata');
+})->middleware(['auth', 'verified'])->name('users');
+Route::middleware('auth')->group(function () {
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::delete('/users', [UserController::class, 'delete'])->name('users.delete');
+    Route::post('/users/update', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/fetchdata', [UserController::class, 'fetchdata'])->name('users.fetchdata');
+});
 
-
+// routes for employee pages
 Route::get('/employee', function () {
     return view('pages.employee'); 
-})->name('employee');
+})->middleware(['auth', 'verified'])->name('employee');
 
+// routes for crops pages
 Route::get('/crops', function () {
     return view('pages.crops'); 
-})->name('crops');
+})->middleware(['auth', 'verified'])->name('crops');
 
+// routes for settings pages
 Route::get('/settings', function () {
     return view('pages.settings'); 
-})->name('settings');
+})->middleware(['auth', 'verified'])->name('settings');
+
+
+
 
 Route::get('users/index', 'App\Http\Controllers\UserController@index')->name('users.index');
 
