4d3f1946fa0c05f832369c8df043e04ed73b3f86
21stfloor <21stfloordev@gmail.com>
2023-11-09 12:25:39 +0800

    removed test users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,20 +20,20 @@ class UsersTableSeeder extends Seeder
             'password' => Hash::make('password123'), // You can set a default password here
         ]);
 
-        $faker = Faker::create();
+        // $faker = Faker::create();
 
-        foreach (range(1, 100) as $index) {
-            // Generate mobile numbers in the format "09123456789"
-            $mobileNumber = '09' . $faker->numberBetween(100000000, 999999999);
+        // foreach (range(1, 100) as $index) {
+        //     // Generate mobile numbers in the format "09123456789"
+        //     $mobileNumber = '09' . $faker->numberBetween(100000000, 999999999);
 
-            DB::table('users')->insert([
-                'name'=> $faker->unique()->name,
-                'email' => $faker->unique()->safeEmail,
-                'user_id' => $faker->userName,
-                'mobile_number' => $mobileNumber,
-                'password' => Hash::make('password123'), // You can set a default password here
-                'role_id' => 1,
-            ]);
-        }
+        //     DB::table('users')->insert([
+        //         'name'=> $faker->unique()->name,
+        //         'email' => $faker->unique()->safeEmail,
+        //         'user_id' => $faker->userName,
+        //         'mobile_number' => $mobileNumber,
+        //         'password' => Hash::make('password123'), // You can set a default password here
+        //         'role_id' => 1,
+        //     ]);
+        // }
     }
 }
